2348e3adfd3d3aed1f10979d8ccfcbf09165b053
Dipendra Jawariya <dipendrajawariya20@gmail.com>
2023-06-04 11:05:11 +0530

    SetUp completed,Now let's Begin

---
+++ Set_matrix_zeros.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	vector<int> row(matrix.size(),0);
+	vector<int> col(matrix[0].size(),0);
+	for(int i = 0 ; i < matrix.size();i++) {
+		for(int j = 0; j < matrix[0].size(); j++) {
+			if(matrix[i][j] == 0) {
+				row[i] = 1;
+				col[j] = 1;
+			}
+		}
+	}
+	for(int i = 0 ; i < matrix.size();i++) {
+		for(int j = 0; j < matrix[0].size(); j++) {
+			if(row[i] == 1 || col[j] == 1) {
+				matrix[i][j] = 0;
+			}
+		}
+	}
+
+}
