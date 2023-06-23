12e5b3f87914ccd6ae41c78c0cc7aad5bbef46de
Dipendra Jawariya <dipendrajawariya20@gmail.com>
2023-06-24 02:02:53 +0530

    Just Completedthe attempted Questions

--- Partial_BST.cpp
+++ Partial_BST.cpp
@@ -27,3 +27,27 @@ bool isValidBST(BinaryTreeNode<int> *root,int minVal,int maxVal) {
 bool validateBST(BinaryTreeNode<int> *root) {
      return isValidBST(root,-1e9, 1e9);
 }
+
+
+
+
+// #include <bits/stdc++.h>
+
+// bool validateBST(BinaryTreeNode<int> *root) {
+//   // Write your code here
+//   if (root == NULL || (root->left == NULL && root->right == NULL))
+//     return true;
+
+//   bool left = validateBST(root->left);
+//   bool right = validateBST(root->right);
+
+//   bool ans = true;
+
+//   if (root->left)
+//     ans = ans && (root->data >= root->left->data);
+
+//   if (root->right)
+//     ans = ans && (root->data <= root->right->data);
+
+//   return ans && left && right;
+// }
